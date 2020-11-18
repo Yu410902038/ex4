6cec80284136dd9b7497c38afdea3cfac13822e5
Yu410902038 <71751927+Yu410902038@users.noreply.github.com>
2020-11-18 15:31:14 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.forever(function () {
+    for (let I = 0; I <= 4; I++) {
+        for (let j = 0; j <= 4; j++) {
+            if (0 + j == 2 && 0 + (0 + 0) == 0) {
+                led.plot(0 - I, 0 - (0 + 0))
+                led.plot(0 - I, 0 + (j + 0))
+            }
+        }
+    }
+})
+basic.forever(function () {
+    for (let I = 0; I <= 4; I++) {
+        for (let j = 0; j <= 4; j++) {
+            if (0 + j == 2 && 0 + (0 + 0) == 0) {
+                led.plot(0 - I, 0 - (0 + 0))
+                led.plot(0 - I, 0 + (j + 0))
+            }
+        }
+    }
+})
